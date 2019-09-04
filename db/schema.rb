9c32d1030df9e02fa525a90e9e6acc00a07d1f34
SakamotoMakishi <dauw.mf81@gmail.com>
2019-09-04 15:58:17 +0900

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_170335) do
+ActiveRecord::Schema.define(version: 2019_09_04_065528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
